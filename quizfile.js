5bd158ce8d1a8aaf3655fd9996b4edd29a9a9f13
nakahirocom <31771074+nakahirocom@users.noreply.github.com>
2021-08-13 22:00:25 +0900

    Update quizfile.js

--- quizfile.js
+++ quizfile.js
@@ -10,14 +10,15 @@ qa = new Array();
 
 
 
-qa[76] = ["HTMLのﾘﾝｸﾀｸﾞは",
-          "aﾀｸﾞ=anchor(ｱﾝｶｰ=船のいかり)",
-          "76_04/94.01-04.さまざまなHTMLﾀｸﾞ_&lt;a href=&quot;https:～～&quot; &gt; &lt;/a&gt; 参考_href(ｴｲﾁﾚﾌ)=hypertext reference(ﾊｲﾊﾟｰﾃｷｽﾄﾘﾌｧﾚﾝｽ=ﾊｲﾊﾟｰﾃｷｽﾄの参照)"];
+qa[6] = ["HTMLのﾘｽﾄﾀｸﾞは",
+          "ulﾀｸﾞ=unordered list(ｱﾝｵｰﾀﾞｰﾄﾞ･ﾘｽﾄ)、olﾀｸﾞ=ordered list（ｵｰﾀﾞｰﾄﾞ･ﾘｽﾄ）",
+          "6_04/94.01-04.さまざまなHTMLﾀｸﾞ_&lt;ul&gt; &lt;li&gt; &lt;/li&gt; &lt;/ul&gt;&nbsp; 参考_li(ｴﾙｱｲ)=list item(ﾘｽﾄｱｲﾃﾑ=項目)。liﾀｸﾞはulﾀｸﾞやolﾀｸﾞの子要素にしなければならない。"];
 
-qa[4] = ["HTMLの段落ﾀｸﾞは",
-          "pﾀｸﾞ=paragraph(ﾊﾟﾗｸﾞﾗﾌ=段落)",
-          "4_04/94.01-04.さまざまなHTMLﾀｸﾞ_&lt;p&gt;&nbsp;&lt;/p&gt; 参考_&lt;br&gt;ﾀｸﾞを文面に入れることで改行できます。br=break(ﾌﾞﾚｰｸ=改行)"];
+qa[5] = ["HTMLの画像ﾀｸﾞは",
+          "imgﾀｸﾞ=image(ｲﾒｰｼﾞ)",
+          "5_04/94.01-04.さまざまなHTMLﾀｸﾞ_&lt;img src=&rdquo;https:～～ &rdquo; alt=&rdquo; &rdquo; /&gt;。参考_src属性=source(ｿｰｽ=出典・由来)  alt属性=alternate(ｵﾙﾀﾈｰﾄ=代替)"];
 
-qa[3] = ["HTMLの見出しﾀｸﾞは",
-          "hﾀｸﾞ=header(ﾍｯﾀﾞｰ=見出し)",
-          "3_04/94.01-04.さまざまなHTMLﾀｸﾞ_&lt;h1&gt; &lt;/h1&gt;&nbsp;参考_&lt;body&gt; &lt;/body&gt;ﾀｸﾞ内に入力します。なお、h1～h6まであります"];
+qa[77] = ["&lt;a href=&quot;https:～～ &quot;&gt; &lt;/a&gt;の要素と属性は",
+          "aタグ=要素 href=属性",
+          "77_04/94.01-04.さまざまなHTMLﾀｸﾞ_href(ｴｲﾁﾚﾌ)属性は、ﾊｲﾊﾟｰﾘﾝｸのﾘﾝｸ先をaﾀｸﾞ要素に情報を与えることができます。<br>なお、href属性はｳｪﾌﾞｻｲﾄへﾘﾝｸ。src属性はﾌｧｲﾙの場所を指定します"];
+
